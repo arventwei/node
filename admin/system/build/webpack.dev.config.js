145c429bb213c026087376b7f3bc535a10161df1
Weixudong <weixudong@bjhike.com>
2018-04-30 21:41:55 +0800

    add

--- admin/system/build/webpack.dev.config.js
+++ admin/system/build/webpack.dev.config.js
@@ -7,7 +7,7 @@ const webpackBaseConfig = require('./webpack.base.config.js');
 const fs = require('fs');
 const package = require('../package.json');
 
-fs.open('./build/env.js', 'w', function(err, fd) {
+fs.open('./env.js', 'w', function(err, fd) {
     const buf = 'export default "development";';
     fs.write(fd, buf, 0, buf.length, 0, function(err, written, buffer) {});
 });
@@ -30,22 +30,9 @@ module.exports = merge(webpackBaseConfig, {
         }),
         new HtmlWebpackPlugin({
             title: 'iView admin v' + package.version,
-            favicon: './td_icon.ico',
             filename: '../index.html',
-            template: '!!ejs-loader!./src/template/index.ejs',
+            template: './src/template/index.ejs',
             inject: false
-        }),
-        new CopyWebpackPlugin([
-            {
-                from: 'src/views/main-components/theme-switch/theme'
-            },
-            {
-                from: 'src/views/my-components/text-editor/tinymce'
-            }
-        ], {
-            ignore: [
-                'text-editor.vue'
-            ]
         })
     ]
 });
