ed41e4b8ef528f186bf6711c3ca1f53643e1be30
Weixudong <weixudong@bjhike.com>
2018-05-01 21:37:24 +0800

    add

--- admin/wechat/config/index.js
+++ admin/wechat/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../../public/wechat/dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/wechat/dist/',
 
     /**
      * Source Maps
